bbe0394cf17b0411f33ee062599201fe327ed9bd
Casey Tran <caseytran4@gmail.com>
2026-09-15 16:39:58 -0700

    fix #56: formating and network naming

--- src/main.ts
+++ src/main.ts
@@ -10,79 +10,77 @@ import { DiscoveryNetwork } from "./util/network/discovery";
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (started) {
-    app.quit();
+	app.quit();
 }
 
 const createWindow = () => {
-    // Create the browser window.
-    const mainWindow = new BrowserWindow({
-        width: 800,
-        height: 480,
-        webPreferences: {
-            preload: path.join(__dirname, "preload", "preload.js"),
-        },
-        fullscreen: app.isPackaged, // enable fullscreen in production
-    });
+	// Create the browser window.
+	const mainWindow = new BrowserWindow({
+		width: 800,
+		height: 480,
+		webPreferences: {
+			preload: path.join(__dirname, "preload", "preload.js"),
+		},
+		fullscreen: app.isPackaged, // enable fullscreen in production
+	});
 
-    const discovery = new DiscoveryNetwork();
-    discovery.start();
+	const discoveryNetwork = new DiscoveryNetwork();
+	startServer(mainWindow, discoveryNetwork);
 
-    startServer(mainWindow);
+	// and load the index.html of the app.
+	if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
+		mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL);
 
-    // and load the index.html of the app.
-    if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
-        mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL);
+		// Open the DevTools.
+		mainWindow.webContents.openDevTools();
+	} else {
+		mainWindow.loadFile(
+			path.join(__dirname, `../.vite/renderer/index.html`),
+		);
+	}
 
-        // Open the DevTools.
-        mainWindow.webContents.openDevTools();
-    } else {
-        mainWindow.loadFile(
-            path.join(__dirname, `../.vite/renderer/index.html`),
-        );
-    }
+	ipcMain.on("close", () => mainWindow.close());
 
-    ipcMain.on("close", () => mainWindow.close());
-
-    initializeSerial(mainWindow);
-    ipcMain.handle(
-        "save-cage",
-        async (event: IpcMainInvokeEvent, cage: CageData) => {
-            try {
-                writeFileSync(
-                    path.join(
-                        app.getPath("appData"),
-                        "god-sensor-cage-config.json",
-                    ),
-                    JSON.stringify(cage),
-                    {
-                        encoding: "utf8",
-                    },
-                );
-                return true;
-            } catch {
-                return false;
-            }
-        },
-    );
-    ipcMain.handle(
-        "load-cage",
-        async (event: IpcMainInvokeEvent): Promise<CageData | null> => {
-            try {
-                const rawData = readFileSync(
-                    path.join(
-                        app.getPath("appData"),
-                        "god-sensor-cage-config.json",
-                    ),
-                    "utf8",
-                );
-                const data = JSON.parse(rawData);
-                const cage = parseCageData.parse(data);
-                return cage;
-            } catch {
-                return null;
-            }
-        },
-    );
+	initializeSerial(mainWindow);
+	ipcMain.handle(
+		"save-cage",
+		async (event: IpcMainInvokeEvent, cage: CageData) => {
+			try {
+				writeFileSync(
+					path.join(
+						app.getPath("appData"),
+						"god-sensor-cage-config.json",
+					),
+					JSON.stringify(cage),
+					{
+						encoding: "utf8",
+					},
+				);
+				return true;
+			} catch {
+				return false;
+			}
+		},
+	);
+	ipcMain.handle(
+		"load-cage",
+		async (event: IpcMainInvokeEvent): Promise<CageData | null> => {
+			try {
+				const rawData = readFileSync(
+					path.join(
+						app.getPath("appData"),
+						"god-sensor-cage-config.json",
+					),
+					"utf8",
+				);
+				const data = JSON.parse(rawData);
+				const cage = parseCageData.parse(data);
+				return cage;
+			} catch {
+				return null;
+			}
+		},
+	);
 };
 
 // This method will be called when Electron has finished
@@ -96,26 +94,26 @@ testingEnvironment();
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on("window-all-closed", () => {
-    if (process.platform !== "darwin") {
-        app.quit();
-    }
+	if (process.platform !== "darwin") {
+		app.quit();
+	}
 });
 
 app.on("activate", () => {
-    // On OS X it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) {
-        createWindow();
-    }
+	// On OS X it's common to re-create a window in the app when the
+	// dock icon is clicked and there are no other windows open.
+	if (BrowserWindow.getAllWindows().length === 0) {
+		createWindow();
+	}
 });
 
 function handleIP(event: IpcMainInvokeEvent, ip: string) {
-    console.log(ip);
-    sendTestQuery(ip);
+	console.log(ip);
+	sendTestQuery(ip);
 }
 
 app.whenReady().then(() => {
-    ipcMain.handle("ip-received", handleIP);
+	ipcMain.handle("ip-received", handleIP);
 });
 
 // In this file you can include the rest of your app's specific main process
--- src/util/ServerQueries.ts
+++ src/util/ServerQueries.ts
@@ -1,146 +1,166 @@
-import express from 'express'
-import { ipcMain, BrowserWindow } from 'electron'
-import os from 'os'
+import os from "os";
+import express from "express";
+import { ipcMain, BrowserWindow } from "electron";
+import { DiscoveryNetwork } from "./network/discovery";
 
-let networkURL = '';
+let networkURL = "";
+let discoveryNetwork;
 
 function getLocalIPAddress() {
-    const interfaces = os.networkInterfaces();
-    const addresses = [];
+	const interfaces = os.networkInterfaces();
+	const addresses = [];
 
-    for (const name of Object.keys(interfaces)) {
-        for (const iface of interfaces[name]) {
-            // Skip internal (loopback) and non-IPv4 addresses
-            if (iface.family === 'IPv4' && !iface.internal) {
-                addresses.push({ name, address: iface.address });
-            }
-        }
-    }
+	for (const name of Object.keys(interfaces)) {
+		for (const iface of interfaces[name]) {
+			// Skip internal (loopback) and non-IPv4 addresses
+			if (iface.family === "IPv4" && !iface.internal) {
+				addresses.push({ name, address: iface.address });
+			}
+		}
+	}
 
-    console.log("from server, addresses:\n",addresses[0]?.address, '\n');
-    return addresses[0]?.address
+	console.log("from server, addresses:\n", addresses[0]?.address, "\n");
+	return addresses[0]?.address;
 }
 
 function mainTest() {
-    console.log('test main')
-    console.log("ServerQueries.ts mainTest() called")
-    console.log();
+	console.log("test main");
+	console.log("ServerQueries.ts mainTest() called");
+	console.log();
 }
 
-function startServer(mainWindow: BrowserWindow) {
-    ipcMain.on('launchMissileRequest', () => {
-        console.log("ServerQueries.ts: received launch missile request from renderer")
-        launchQuery();
-    })
-    ipcMain.on('launcherLocPing', (event, x: number, y: number) => {
-        console.log(`ServerQueries.ts: received launcher location ping from renderer: x=${x}, y=${y}`)
-        launcherLocQuery(x, y);
-        // Here you can handle the x and y coordinates as needed
-    })
-    ipcMain.on('LOS_LocPing', (event, x: number, y: number) => {
-        console.log(`ServerQueries.ts: received launcher LOS location ping from renderer: x=${x}, y=${y}`)
-        LOS_LocQuery(x, y);
-        // Here you can handle the x and y coordinates as needed
-    })
-    const ip = getLocalIPAddress()
-    const server = express()
-    server.use(express.json())
-    const port = 3000
+function startServer(mainWindow: BrowserWindow, discovery: DiscoveryNetwork) {
+	discoveryNetwork = discovery;
 
-    server.listen(port, () => {
-        console.log(`ServerQueries.ts: Server is running on ${ip}:${port}`)
-    })
+	ipcMain.on("launchMissileRequest", () => {
+		console.log(
+			"ServerQueries.ts: received launch missile request from renderer",
+		);
+		launchQuery();
+	});
+	ipcMain.on("launcherLocPing", (event, x: number, y: number) => {
+		console.log(
+			`ServerQueries.ts: received launcher location ping from renderer: x=${x}, y=${y}`,
+		);
+		launcherLocQuery(x, y);
+		// Here you can handle the x and y coordinates as needed
+	});
+	ipcMain.on("LOS_LocPing", (event, x: number, y: number) => {
+		console.log(
+			`ServerQueries.ts: received launcher LOS location ping from renderer: x=${x}, y=${y}`,
+		);
+		LOS_LocQuery(x, y);
+		// Here you can handle the x and y coordinates as needed
+	});
+	const ip = getLocalIPAddress();
+	const server = express();
+	server.use(express.json());
+	const port = 3000;
 
-    server.get('/', (req, res) => {
-        res.send('Hello from RED GUI server!')
-    })
-    server.post('/droneLoc', (req, res) => {
-        res.send('ok')
-        console.log("ServerQueries.ts: received drone location ping from Blue GUI")
-        const data = req.body;
-        console.log(data); // Log the received dataping from Blue GUI
-        mainWindow.webContents.send('droneLocPing', data) // Forward the data to the renderer process
-    })
-    server.post('/missileLoc', (req, res) => {
-        res.send('ok')
-        console.log("ServerQueries.ts: received missile location ping from Blue GUI")
-        const data = req.body;
-        console.log(data);
-        mainWindow.webContents.send('missileLocPing', data) // Forward the data to the renderer process
-    })
+	server.listen(port, () => {
+		console.log(`ServerQueries.ts: Server is running on ${ip}:${port}`);
+	});
 
-    testQuery();
+	server.get("/", (req, res) => {
+		res.send("Hello from RED GUI server!");
+	});
+	server.post("/droneLoc", (req, res) => {
+		res.send("ok");
+		console.log(
+			"ServerQueries.ts: received drone location ping from Blue GUI",
+		);
+		const data = req.body;
+		console.log(data); // Log the received dataping from Blue GUI
+		mainWindow.webContents.send("droneLocPing", data); // Forward the data to the renderer process
+	});
+	server.post("/missileLoc", (req, res) => {
+		res.send("ok");
+		console.log(
+			"ServerQueries.ts: received missile location ping from Blue GUI",
+		);
+		const data = req.body;
+		console.log(data);
+		mainWindow.webContents.send("missileLocPing", data); // Forward the data to the renderer process
+	});
 
-    console.log("ServerQueries.ts: startServer() END\n")
+	testQuery();
+
+	console.log("ServerQueries.ts: startServer() END\n");
 }
 
 async function testQuery() {
-    // Using this testURL because it's free and open
-    const testURL = 'https://jsonplaceholder.typicode.com/todos/1'
-    const response = await fetch(testURL);
-    const data = await response.json();
-    console.log(data);
-    console.log("ServerQueries.ts: testQuery() END\n")
+	// Using this testURL because it's free and open
+	const testURL = "https://jsonplaceholder.typicode.com/todos/1";
+	const response = await fetch(testURL);
+	const data = await response.json();
+	console.log(data);
+	console.log("ServerQueries.ts: testQuery() END\n");
 }
 
 async function sendTestQuery(ip: string) {
-    const bluePort = 3003
-    const url = `http://${ip}:${bluePort}/`;
-    console.log(url)
-    try {
-        const response = await fetch(url);
-        const data = await response.text();
-        console.log(data);
-        networkURL = url;
-    } catch (error) {
-        console.log(error);
-    }
+	const bluePort = 3003;
+	const url = `http://${ip}:${bluePort}/`;
+	console.log(url);
+	try {
+		const response = await fetch(url);
+		const data = await response.text();
+		console.log(data);
+		networkURL = url;
+	} catch (error) {
+		console.log(error);
+	}
 }
 
 // url will need to be changed for PROD
 async function launchQuery() {
-    const localhost_url = 'http://localhost:3003/pingMissileLaunch';
-    try {
-        let targetURL = localhost_url;
-        if (networkURL != '') { targetURL = `${networkURL}pingMissileLaunch`; }
-        await fetch(targetURL)
-    } catch (error) {
-        console.error("Error in launchQuery():", error);
-    }
+	const localhost_url = "http://localhost:3003/pingMissileLaunch";
+	try {
+		let targetURL = localhost_url;
+		if (networkURL != "") {
+			targetURL = `${networkURL}pingMissileLaunch`;
+		}
+		await fetch(targetURL);
+	} catch (error) {
+		console.error("Error in launchQuery():", error);
+	}
 }
 
 async function launcherLocQuery(x: number, y: number) {
-    const localhost_url = `http://localhost:3003/pingLauncherLoc`;
-    const payload = { x, y };
-    try {
-        let targetURL = localhost_url;
-        if (networkURL != '') { targetURL = `${networkURL}pingLauncherLoc`; }
-        console.log(`Sending launcher location to ${targetURL}`);
-        await fetch(targetURL, {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(payload)
-        })
-    } catch (error) {
-        console.error("Error in launcherLocQuery():", error);
-    }
+	const localhost_url = `http://localhost:3003/pingLauncherLoc`;
+	const payload = { x, y };
+	try {
+		let targetURL = localhost_url;
+		if (networkURL != "") {
+			targetURL = `${networkURL}pingLauncherLoc`;
+		}
+		console.log(`Sending launcher location to ${targetURL}`);
+		await fetch(targetURL, {
+			method: "POST",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify(payload),
+		});
+	} catch (error) {
+		console.error("Error in launcherLocQuery():", error);
+	}
 }
 
 async function LOS_LocQuery(x: number, y: number) {
-    const localhost_url = `http://localhost:3003/pingLOSLoc`;
-    const payload = { x, y };
-    try {
-        let targetURL = localhost_url;
-        if (networkURL != '') { targetURL = `${networkURL}pingLauncherLoc`; }
-        console.log(`Sending launcher location to ${targetURL}`);
-        await fetch(targetURL, {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(payload)
-        })
-    } catch (error) {
-        console.error("Error in launcherLocQuery():", error);
-    }
+	const localhost_url = `http://localhost:3003/pingLOSLoc`;
+	const payload = { x, y };
+	try {
+		let targetURL = localhost_url;
+		if (networkURL != "") {
+			targetURL = `${networkURL}pingLauncherLoc`;
+		}
+		console.log(`Sending launcher location to ${targetURL}`);
+		await fetch(targetURL, {
+			method: "POST",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify(payload),
+		});
+	} catch (error) {
+		console.error("Error in launcherLocQuery():", error);
+	}
 }
 
-export {mainTest, startServer, testQuery, sendTestQuery, launchQuery}
+export { mainTest, startServer, testQuery, sendTestQuery, launchQuery };
